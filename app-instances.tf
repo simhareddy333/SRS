053b6e0748d95d60d636cb88c2459f1f087662cd
simhareddy333 <48729755+simhareddy333@users.noreply.github.com>
2020-05-06 23:01:20 +0530

    Add files via upload

---
+++ app-instances.tf
@@ -0,0 +1,65 @@
+/* Terraform code for Creating teh Docker Swarm Cluster */
+
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
+resource "aws_instance" "manager" {
+  ami           = "ami-26c43149"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.swarm.name}"]
+  key_name = "${aws_key_pair.deployer.key_name}"
+  connection {
+    user = "ubuntu"
+    key_file = "ssh/key"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install apt-transport-https ca-certificates",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-engine=1.12.0",
+      "sudo docker swarm init",
+      "sudo docker swarm join-token --quiet worker > /home/ubuntu/token"
+    ]
+  }
+  provisioner "file" {
+    source = "proj"
+    destination = "/home/ubuntu/"
+  }
+  tags = { 
+    Name = "swarm-manager"
+  }
+}
+
+resource "aws_instance" "node" {
+  count         = 4
+  ami           = "ami-26c43149"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.swarm.name}"]
+  key_name = "${aws_key_pair.deployer.key_name}"
+  connection {
+    user = "ubuntu"
+    key_file = "ssh/key"
+  }
+  provisioner "file" {
+    source = "key.pem"
+    destination = "/home/ubuntu/key.pem"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install apt-transport-https ca-certificates",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-engine",
+      "sudo chmod 400 /home/ubuntu/test.pem",
+      "sudo scp -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -o UserKnownHostsFile=/dev/null -i test.pem ubuntu@${aws_instance.master.private_ip}:/home/ubuntu/token .",
+      "sudo docker swarm join --token $(cat /home/ubuntu/token) ${aws_instance.master.private_ip}:2377"
+    ]
+  }
+  tags = { 
+    Name = "swarm-${count.index}"  /* will be using variable to sequence the nodes as node1,node2...etc */
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+	default = "ACCESS_KEY"
+}
+variable "secret_key" {
+	default = "SECRET_KEY"
+}
+variable "region" {
+    default = "us-east-1"
+}
